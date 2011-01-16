5f8be2d0969b77716368dc0126b8e2438b2736dd
Taylor Hedberg <tmhedberg@gmail.com>
2011-01-16 03:25:42 -0500

    sigtrap code

---
+++ sigtrap.c
@@ -0,0 +1,99 @@
+/*
+ * Do nothing until the specified signal is received, then exec to the specified
+ * command
+ *
+ * Usage: sigtrap <signal> <command> [arguments...]
+ *
+ * Supported signals: USR1, USR2, HUP, INT, ILL, BUS, FPE, URG, PWR, SYS
+ *
+ * Compile with: gcc -o sigtrap sigtrap.c -liberty
+ *
+ * Author: Taylor Hedberg <tmhedberg@gmail.com>
+ */
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <libiberty.h>
+
+static char **cmd_argv;
+
+void str_toupper(char *str)
+{
+	while (*str != '\0') {
+		*str = toupper(*str);
+		++str;
+	}
+}
+
+void handle(const int sig)
+{
+	static const char *err_msg_pre = "Couldn't exec to ";
+
+	char *err_msg;
+	sigset_t *set;
+
+	printf("Received signal %d\n", sig);
+
+	set = (sigset_t *)malloc(sizeof (sigset_t));
+	sigemptyset(set);
+	if (sigaddset(set, sig)) {
+		perror("Couldn't add signal to unblock set");
+		exit(EXIT_FAILURE);
+	}
+	if (sigprocmask(SIG_UNBLOCK, set, NULL)) {
+		perror("Couldn't modify signal mask");
+		exit(EXIT_FAILURE);
+	}
+
+	execvp(cmd_argv[0], cmd_argv);
+
+	err_msg = (char *)malloc(strlen(err_msg_pre) + strlen(cmd_argv[0]));
+	strcpy(err_msg, err_msg_pre);
+	strcat(err_msg, cmd_argv[0]);
+	perror(err_msg);
+	exit(EXIT_FAILURE);
+}
+
+int main(const int argc, char *argv[])
+{
+	struct sigaction *sact;
+	char *signame;
+	size_t signame_len;
+	int signum;
+
+	if (argc < 3) {
+		fprintf(stderr, "Usage: sigtrap <signal> <command> [arguments...]\n");
+		exit(EXIT_FAILURE);
+	}
+
+	str_toupper(argv[1]);
+	if (!strcmp(argv[1], "USR1"))
+		signum = SIGUSR1;
+	else if (!strcmp(argv[1], "USR2"))
+		signum = SIGUSR2;
+	else {
+		signame_len = sizeof argv[1] + 3;
+		signame = (char *)malloc(signame_len);
+		snprintf(signame, signame_len, "SIG%s", argv[1]);
+		signum = strtosigno(signame);
+		free(signame);
+	}
+	cmd_argv = &argv[2];
+
+	sact = (struct sigaction *)malloc(sizeof (struct sigaction));
+	sact->sa_handler = handle;
+	sigemptyset(&(sact->sa_mask));
+	sact->sa_flags = SA_RESETHAND;
+	if (sigaction(signum, sact, NULL)) {
+		perror("Couldn't register signal handler");
+		exit(EXIT_FAILURE);
+	}
+	free(sact);
+
+	printf("Waiting for SIG%s...\n", argv[1]);
+	pause();
+}
